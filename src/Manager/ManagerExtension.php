f20925c375c191ac90871bcf71c5f71fae34db08
Dan Linn <dan@helloworlddevs.com>
2024-08-24 13:35:43 -0700

    minor api changes

--- src/Manager/ManagerExtension.php
+++ src/Manager/ManagerExtension.php
@@ -3,6 +3,7 @@
 namespace HelloWorldDevs\Behat\Manager;
 
 use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
+use Behat\Testwork\ServiceContainer\ExtensionManager;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
