1169c1a51e4f537aca08d6bc8b00fa458eeb9da0
Dan Linn <dan@helloworlddevs.com>
2024-08-24 13:18:48 -0700

    adding extension file:

---
+++ src/Manager/ManagerExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HelloWorldDevs\Behat\Manager;
+
+use Behat\Testwork\ServiceContainer\Extension as ExtensionInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class ManagerExtension implements ExtensionInterface
+{
+  public function getConfigKey()
+  {
+    return 'manager_extension';
+  }
+
+  public function initialize(ExtensionManager $extensionManager) {}
+
+  public function configure(ArrayNodeDefinition $builder) {}
+
+  public function load(ContainerBuilder $container, array $config)
+  {
+    $definition = new Definition('HelloWorldDevs\Behat\Manager\CustomDrupalAuthenticationManager');
+    $container->setDefinition('manager_extension.authentication_manager', $definition);
+  }
+
+  public function process(ContainerBuilder $container) {}
+}
